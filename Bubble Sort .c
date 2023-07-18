57e02cd120cd42ea3f2bf33af131f3e934ba472b
Sanjay.R <133894028+Sanjayr4@users.noreply.github.com>
2023-07-18 20:49:42 +0530

    Add files via upload

---
+++ Bubble Sort .c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    // To Write C code here for bubble sort bubble sort
+    int n;
+    scanf ("%d",&n);
+    int arr[n];
+    for (int i=0;i<n;i++)
+    {
+        scanf ("%d",&arr[i]);
+    }
+    for (int i=0;i<n;i++)// thise for acessing the index in array
+    {
+       for (int j=0;j<n-1;j++)// thise for ascessing the number in that index
+       {
+           if (arr[j]>arr[j+1])
+           {
+               //swapping process
+               int temp = arr[j];
+               arr[j] = arr[j+1];
+               arr[j+1] = temp;
+               
+           }
+       }
+    }
+    for (int i=0;i<n;i++)
+    {
+        printf ("arr[%d] %d\n ",i,arr[i]);
+        // The higher the index higher the number
+    }
+
+    return 0;
+}
